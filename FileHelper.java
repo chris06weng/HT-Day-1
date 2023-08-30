fa85738395e366e7243b87705313976a37cfe997
Chris <chris.r.weng@gmail.com>
2023-08-30 12:20:11 -0700

    Fixed everything
    Staged Bari's changes and fixed the code to work on all methods and tester

--- FileHelper.java
+++ FileHelper.java
@@ -1,23 +1,16 @@
-<<<<<<< Updated upstream
-import java.io.*;
-import java.util.*;
-=======
+/*impor java.io.*;
+import java.util.*;*/
+
 import java.io.BufferedReader;
-import java.io.PrintWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
->>>>>>> Stashed changes
+import java.io.PrintWriter;
 
 public class FileHelper {
-    boolean exists;
 
-    public FileHelper() {
-        exists = true;
-    }
-
-    public void stringToFile(String fileName, String input) throws FileNotFoundException {
+    public static void stringToFile(String fileName, String input) throws FileNotFoundException {
         File file = new File(fileName);
         PrintWriter pw = new PrintWriter(file);
         for (int k = 0; k < input.length(); k++) {
@@ -26,53 +19,30 @@ public class FileHelper {
         pw.close();
     }
 
-<<<<<<< Updated upstream
-    public String fileToString(File fileName) throws IOException {
+    public static String fileToString(File fileName) throws IOException {
         StringBuilder record = new StringBuilder("");
         BufferedReader text = new BufferedReader(new FileReader(fileName));
 
         while (text.ready()) {
             record.append((char) text.read());
-=======
-    public String fileToString(String fileName) throws FileNotFoundException, IOException {
-        String record = "";
-        BufferedReader text = new BufferedReader(new FileReader(fileName));
-
-        while (text.ready()) {
-            record += (char) (text.read());
->>>>>>> Stashed changes
         }
 
         text.close();
         return record.toString();
     }
-<<<<<<< Updated upstream
 
-    public int countCharacters(String fileName) {
+    public static int countCharacters(String fileName) throws FileNotFoundException, IOException {
+        int counter = 0;
+        File file = new File(fileName);
+        BufferedReader br = new BufferedReader(new FileReader(file));
 
-        try {
-
-            FileReader fileReader = new FileReader(fileName);
-            BufferedReader br = new BufferedReader(fileReader);
-            StringBuilder record = new StringBuilder("");
-
-            String output = "";
-
-            output += br.readLine();
-
-            return output.length();
-
+        while (br.ready()) {
+            counter++;
+            br.read();
+            ;
         }
 
-        catch (Exception e) {
-
-            System.out.println("Make sure you type the file name correctly!");
-
-            System.out.println(e);
-            return 0;
-        }
-
+        br.close();
+        return counter; 
     }
-=======
->>>>>>> Stashed changes
 }
--- FileHelperTester.java
+++ FileHelperTester.java
@@ -1,26 +1,12 @@
-<<<<<<< Updated upstream
 import java.io.File;
-=======
->>>>>>> Stashed changes
 import java.io.IOException;
 
 public class FileHelperTester {
     public static void main(String[] args) throws IOException {
-        FileHelper fh = new FileHelper();
-        fh.stringToFile("filetester.txt", "abcdefg");
-<<<<<<< Updated upstream
-        File file = new File("filetester.txt");
+        FileHelper.stringToFile("filetester.txt", "abcdefg");
+        File file = new File("filetester2.txt");
 
-        System.out.println(fh.fileToString(file));
-
-        System.out.println(fh.countCharacters("filetester.txt"));
-        System.out.println(fh.countCharacters("hello2.txt"));
-
+        System.out.println(FileHelper.countCharacters("hello.txt"));
     }
 
-=======
-        String test = fh.fileToString("filetester2.txt");
-        System.out.println(test);
-    }
->>>>>>> Stashed changes
 }
